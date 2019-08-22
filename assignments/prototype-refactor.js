a09dccd76afa83501c1c019035827a7855b23373
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-08-22 19:19:38 +0100

    created a class of person

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,33 +4,33 @@
 //1. Copy and paste your code or the solution from yesterday
 
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.stomach = [];
-}
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.stomach = [];
+// }
 
-Person.prototype.greet = function () {
-  return this.name + " is " + this.age + " years old"
-}
+// Person.prototype.greet = function () {
+//   return this.name + " is " + this.age + " years old"
+// }
 
-Person.prototype.eat = function (food) {
-  this.stomach.push(food)
-}
+// Person.prototype.eat = function (food) {
+//   this.stomach.push(food)
+// }
 
-Person.prototype.poop = function () {
-  this.stomach = []
-}
+// Person.prototype.poop = function () {
+//   this.stomach = []
+// }
 
 
-let dammy = new Person("Damilola", 48);
-console.log(dammy.greet())
-dammy.eat("rice")
+// let dammy = new Person("Damilola", 48);
+// console.log(dammy.greet())
+// dammy.eat("rice")
 
-console.log(dammy.stomach)
+// console.log(dammy.stomach)
 
-dammy.poop();
-console.log(dammy.stomach)
+// dammy.poop();
+// console.log(dammy.stomach)
 
 
 // TASK 2
@@ -43,35 +43,35 @@ console.log(dammy.stomach)
 // - Give cars the ability to be repaired.
 // - A repaired car can be driven again.
 
-function Car(name, make) {
-  this.name = name;
-  this.make = make;
-  this.odometer = 0;
-  this.crashed = false;
-}
+// function Car(name, make) {
+//   this.name = name;
+//   this.make = make;
+//   this.odometer = 0;
+//   this.crashed = false;
+// }
 
-Car.prototype.drive = function (distance) {
-  if (this.crashed === true) {
-    return;
-  }
-  this.odometer = this.odometer + distance
-}
+// Car.prototype.drive = function (distance) {
+//   if (this.crashed === true) {
+//     return;
+//   }
+//   this.odometer = this.odometer + distance
+// }
 
-Car.prototype.repair = function () {
-  this.crashed = false;
-}
+// Car.prototype.repair = function () {
+//   this.crashed = false;
+// }
 
-Car.prototype.crash = function () {
-  this.crashed = true;
-  return `I crashed at ${this.odometer} miles!`;
-}
+// Car.prototype.crash = function () {
+//   this.crashed = true;
+//   return `I crashed at ${this.odometer} miles!`;
+// }
 
 
-let picanto = new Car("Picanto", 'Kia');
-picanto.drive(40);
+// let picanto = new Car("Picanto", 'Kia');
+// picanto.drive(40);
 
-console.log(picanto)
-picanto.drive(20);
+// console.log(picanto)
+// picanto.drive(20);
 
 
 // TASK 3
@@ -81,33 +81,123 @@ picanto.drive(20);
 // - Babies should have the ability to play, which persons don't.
 // - By playing, a string is returned with some text of your choosing.
 
-function Baby(babyAttributes) {
-  Person.call(this, babyAttributes); // binding this to Person
-}
-Baby.prototype = Object.create(Person.prototype);
-Baby.prototype.play = function () {
-  return this.name + " is playing"
-}
+// function Baby(babyAttributes) {
+//   Person.call(this, babyAttributes); // binding this to Person
+// }
+// Baby.prototype = Object.create(Person.prototype);
+// Baby.prototype.play = function () {
+//   return this.name + " is playing"
+// }
 
-let baby = new Baby("Peter", 40)
-baby.play()
-  // TASK 4
+// let baby = new Baby("Peter", 40)
+// baby.play()
+// TASK 4
 
-  // Use your imagination and come up with constructors that allow to build objects
-  // With amazing and original capabilities. Build 3 small ones, or a very
-  // complicated one with lots of state. Surprise us!
+// Use your imagination and come up with constructors that allow to build objects
+// With amazing and original capabilities. Build 3 small ones, or a very
+// complicated one with lots of state. Surprise us!
 
 
-  function Truck(truckAttributes) {
-    Car.call(this, truckAttributes); // binding this to Car;
-    this.load = 0
-  }
-  Truck.prototype = Object.create(Car.prototype);
-  Truck.prototype.carryLoad = function (bags) {
-    this.load = bags
-  } 
+//   function Truck(truckAttributes) {
+//     Car.call(this, truckAttributes); // binding this to Car;
+//     this.load = 0
+//   }
+//   Truck.prototype = Object.create(Car.prototype);
+//   Truck.prototype.carryLoad = function (bags) {
+//     this.load = bags
+//   } 
 
 
 
 //2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+//TASK 1
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.stomach = [];
+    }
 
+    greet = () => {
+        return this.name + " is " + this.age + " years old"
+    }
+    eat = (food) => {
+        this.stomach.push(food)
+    }
+    poop = (food) => {
+        this.stomach.pop();
+    }
+}
+
+const dammy = new Person("Damilola", 48);
+let tobe = new Person('Tobechukwu',56)
+
+let damigreeting = dammy.greet();
+console.log(damigreeting)
+
+
+//TASK 2
+class Car {
+    constructor(name, make) {
+        this.name = name;
+        this.make = make;
+        this.odometer = 0;
+        this.crashed = false;
+    }
+
+    drive = (distance) => {
+        if (this.crashed === true) {
+            return;
+        }
+        this.odometer = this.odometer + distance
+    }
+
+    repair = () => {
+        this.crashed = false;
+    }
+
+    crash = () => {
+        this.crashed = true;
+        return `I crashed at ${this.odometer} miles!`;
+    }
+
+}
+
+
+let picanto = new Car("Picanto", 'Kia');
+picanto.drive(40);
+
+console.log(picanto.odometer)
+picanto.drive(20);
+console.log(picanto.odometer)
+
+//TASK 3
+
+class Baby extends Person {
+    constructor(name, age) {
+        super(name, age)
+    }
+
+    play = () => {
+        return this.name + " is playing"
+    }
+
+    cry = () => {
+        return this.name + " is crying"
+    }
+}
+
+let akin = new Baby ('akin', 3) 
+console.log(akin.greet())
+
+//TASK 4
+class Truck extends Car {
+    constructor(attributes) {
+        super(attributes)
+        this.load = 0
+    }
+
+    carryLoad = (bags) => {
+        this.load = bags;
+    }
+}
